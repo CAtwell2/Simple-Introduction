9477f9c9e13d176c2e27df7737f4702f3f2c683d
Caden Atwell <99625358+CAtwell2@users.noreply.github.com>
2022-08-24 21:29:52 -0400

    Add files via upload

---
+++ Introduction.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("What is your first name?");
+string firstName = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("What is your last name?");
+string lastName = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("How old are you?");
+string age = Console.ReadLine();
+Console.WriteLine();
+string firstnameinitial = firstName.Substring(0, 1);
+string lastnameinitial = lastName.Substring(0, 1);
+Console.WriteLine($"Hello {firstName} {lastName}! Your initials {firstnameinitial}.{lastnameinitial}., and you are {age}");
+
